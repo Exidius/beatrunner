88402fac634ac5f1ee3869c43b7525b2c2c4a817
Exidius <barad95@gmail.com>
2021-03-27 13:32:26 +0100

    .

--- app/src/main/java/com/barad/beatrunner/MainActivity.kt
+++ app/src/main/java/com/barad/beatrunner/MainActivity.kt
@@ -2,7 +2,12 @@ package com.barad.beatrunner
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -10,17 +15,17 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.lifecycle.Observer
 import com.barad.beatrunner.data.MusicStore
-import com.barad.beatrunner.models.Music
-import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.PlaybackParameters
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.ui.PlayerControlView
-import kotlinx.coroutines.*
+import java.time.Instant
+import java.time.LocalDateTime
+import java.util.*
+import kotlin.math.sqrt
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private lateinit var playerView: PlayerControlView
     private lateinit var player: SimpleExoPlayer
@@ -29,6 +34,20 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btn_reset: Button
     private lateinit var tv_tempo: TextView
     private var speed = 1.0F
+
+    private lateinit var tv_gyro: TextView
+    private lateinit var tv_gyro2: TextView
+    private lateinit var tv_gyro3: TextView
+    private lateinit var sensorManager: SensorManager
+    private lateinit var sensorGyro: Sensor
+    private lateinit var sensorAcc: Sensor
+    private lateinit var sensorLinAcc: Sensor
+
+    val sensorQueue: Queue<Float> = LinkedList()
+    val timeQueue: Queue<Instant> = LinkedList()
+
+    private var asdText = ""
+    private var latest = Instant.now()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,7 +64,7 @@ class MainActivity : AppCompatActivity() {
                 Log.d("barad-log-main", "-----------------")
             })
 
-            musicStore.getAllMusicFromDevice(this,true)
+            //musicStore.getAllMusicFromDevice(this,true)
 
             player = SimpleExoPlayer.Builder(this).build()
             //val mediaItem: MediaItem = MediaItem.fromUri(musicList[2].uri)
@@ -73,8 +92,33 @@ class MainActivity : AppCompatActivity() {
                 speed += 0.1F
                 player.setPlaybackParameters(PlaybackParameters(speed))
             }
+
+            tv_gyro = findViewById(R.id.tv_gyro)
+            tv_gyro2 = findViewById(R.id.tv_gyro2)
+            tv_gyro3 = findViewById(R.id.tv_gyro3)
+            sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+            sensorGyro = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+            sensorAcc = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+            sensorLinAcc = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+
         }
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // DELAY
+        sensorManager.registerListener(this,sensorGyro,SensorManager.SENSOR_DELAY_FASTEST)
+        sensorManager.registerListener(this,sensorAcc,SensorManager.SENSOR_DELAY_FASTEST)
+        sensorManager.registerListener(this,sensorLinAcc,SensorManager.SENSOR_DELAY_FASTEST)
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        sensorManager.unregisterListener(this)
     }
 
     @SuppressLint("ObsoleteSdkInt")
@@ -95,5 +139,55 @@ class MainActivity : AppCompatActivity() {
         } else {
             true
         }
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event != null) {
+            val sensorType: Int = event.sensor.type
+
+            when (sensorType) {
+                Sensor.TYPE_ACCELEROMETER -> {
+                    val x: Float = event.values[0]
+                    val y: Float = event.values[1]
+                    val z: Float = event.values[2]
+
+                    sensorQueue.add(sqrt(x*x+y*y+z*z))
+                    if (sensorQueue.average() > 20) {
+                        var lel = Instant.now().toEpochMilli() - latest.toEpochMilli()
+                        if (lel > 150) {
+                            latest = Instant.now()
+                            timeQueue.add(Instant.now())
+                            asdText = ""
+                            timeQueue.forEach { asdText += it.toString() + " | " }
+                            Log.d("brd-1", timeQueue.size.toString())
+                            Log.d("brd-2", asdText)
+                            tv_gyro2.setText(timeQueue.size.toString())
+                        }
+                    }
+
+                    if (sensorQueue.size > 10) { sensorQueue.remove() }
+                    if (timeQueue.size > 10) { timeQueue.remove() }
+
+                    if (timeQueue.size > 5) {
+                        val list:List<Instant> = timeQueue.map { x -> x }
+                        var sum: Long = 0
+                        for(i in 1 until list.size) {
+                            sum += list[i].toEpochMilli()-list[i-1].toEpochMilli()
+                        }
+                        tv_gyro3.setText((60f/(sum/list.size)*1000f).toString())
+                    }
+
+                    tv_gyro.setText("%.2f".format(x) + " / " + "%.2f".format(y) + " / " + "%.2f".format(z))
+                }
+                else -> {
+                }
+            }
+        }
+
+
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
     }
 }
